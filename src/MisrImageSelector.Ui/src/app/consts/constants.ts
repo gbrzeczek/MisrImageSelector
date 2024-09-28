77bf6cc0c1e5c6d497869d67c976e64fae338f65
Grzegorz Brzeczek <gbrzeczek@dtiq.com>
2024-09-28 21:09:19 +0200

    fix: reverted local api url

--- src/MisrImageSelector.Ui/src/app/consts/constants.ts
+++ src/MisrImageSelector.Ui/src/app/consts/constants.ts
@@ -1,6 +1,5 @@
 
 export class Constants {
-    // public static readonly apiUrl = 'https://gbrzeczek.dev';
-    public static readonly apiUrl = 'http://localhost:5000';
+    public static readonly apiUrl = 'https://gbrzeczek.dev';
     public static readonly totalImages = 232;
 }
