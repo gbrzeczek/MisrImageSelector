035de3f5c79dc101e6fb53b3c24c3f34c5accb2a
Grzegorz Brzeczek <gbrzeczek@dtiq.com>
2024-09-28 21:02:18 +0200

    chore: upgraded to dynamic_2

--- src/MisrImageSelector.Ui/src/app/consts/constants.ts
+++ src/MisrImageSelector.Ui/src/app/consts/constants.ts
@@ -1,5 +1,6 @@
 
 export class Constants {
-    public static readonly apiUrl = 'https://gbrzeczek.dev';
+    // public static readonly apiUrl = 'https://gbrzeczek.dev';
+    public static readonly apiUrl = 'http://localhost:5000';
     public static readonly totalImages = 232;
 }
